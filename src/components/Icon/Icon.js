8b23f173f1615ef93009186a60b0975ee0dfe753
Yannick Longval <yannicklongval2014@gmail.com>
2023-03-05 12:49:09 -0500

    setting up gh-pages

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -2,7 +2,7 @@ import './Icon.css';
 
 function Icon(props) {
   return (
-    <a href={props.href}>
+    <a href={props.href} target="blank">
     <div className="Icon">
         <img src={"./icons/" + props.name}/>
     </div>

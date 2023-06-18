ae12ad1d93344c383a5244c77a1663fa06284e20
YannickLongval <yannicklongval2014@gmail.com>
2023-06-17 20:07:38 -0400

    dark mode

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -13,10 +13,31 @@ window.addEventListener("scroll", function(){
   }
 });
 
+let state = "light"
+
+let changeMode = () => {
+  var r = document.querySelector(':root')
+  if(state === "light"){
+    state = "dark"
+    r.style.setProperty('--text-primary', '#F1F6F9');
+    r.style.setProperty('--bg-lighter', '#303d5a');
+    r.style.setProperty('--bg-primary', '#394867');
+    r.style.setProperty('--bg-darker', '#212A3E');
+    r.style.setProperty('--action-primary', '#212A3E');
+  } else {
+    state = "light"
+    r.style.setProperty('--text-primary', '#212A3E');
+    r.style.setProperty('--bg-lighter', '#ffffff');
+    r.style.setProperty('--bg-primary', '#F1F6F9');
+    r.style.setProperty('--bg-darker', '#e3ebef');
+    r.style.setProperty('--action-primary', '#394867');
+  }
+}
+
 function NavBar() {
   return (
     <div className="navbar">
-        <img/>
+        <button type="button" onClick={changeMode}>CHANGE MODE</button>
         <ul className="navbarOptions">
           <li><a href="#home">Home</a></li>
           <li><a href="#welcome">Welcome</a></li>

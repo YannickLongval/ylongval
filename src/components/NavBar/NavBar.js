82a8426cf3299cb43974245bd4c889963cad26f4
Yannick Longval <90875304+YannickLongval@users.noreply.github.com>
2023-06-21 18:18:49 -0400

    hamburger meny

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -36,15 +36,27 @@ let changeMode = () => {
   }
 }
 
+let uncheck = () => {
+  document.getElementById("menu-toggle").checked = false
+}
+
 function NavBar() {
   return (
     <div className="navbar">
         <button type="button" onClick={changeMode} id='toggle'>☀</button>
+        <input id="menu-toggle" type="checkbox" />
+          <label className='menu-button-container' htmlFor="menu-toggle">
+            <div className='navbarButton'>
+              <span className='menuBarTop'></span>
+              <span className='menuBarCenter'></span>
+              <span className='menuBarBottom'></span>
+            </div>
+          </label>
         <ul className="navbarOptions">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#welcome">Welcome</a></li>
-          <li><a href="#about">Me</a></li>
-          <li><a href="#projects">Projects</a></li>
+          <li><a href="#home" onClick={uncheck}>Home</a></li>
+          <li><a href="#welcome" onClick={uncheck}>Welcome</a></li>
+          <li><a href="#about" onClick={uncheck}>Me</a></li>
+          <li><a href="#projects" onClick={uncheck}>Projects</a></li>
         </ul>
     </div>
   );
